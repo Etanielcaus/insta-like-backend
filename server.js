b3c12b5e97d3b3e8f3cc955a28655d2332030333
Etanielcaus <nielfilosofia@gmail.com>
2024-11-20 00:28:41 -0300

    Criando e Estruturando sua Primeira API com GET e Banco de Dados

--- server.js
+++ server.js
@@ -2,10 +2,57 @@ import express from "express";
 
 const app = express();
 
+const posts = [
+    {
+        id: 1,
+      descricao: "Free Fire",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    },
+    {
+        id: 2,
+      descricao: "Among Us",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    },
+    {
+        id: 3,
+      descricao: "Clash of Clans",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    },
+    {
+        id: 4,
+      descricao: "Subway Surfers",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    },
+    {
+        id: 5,
+      descricao: "Candy Crush Saga",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    },
+    {
+        id: 6,
+      descricao: "Pokémon GO",
+      imagem: "https://cdn.awsli.com.br/2500x2500/129/129579/produto/76899303/e75a7339f1.jpg"
+    }
+  ];
+
+
+  app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando na porta 3000...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("OK");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostID(id) {
+    return posts.findIndex((post) => {
+        return post.id == Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostID(req.params.id)
+    res.status(200).json(posts[index]);
 });
